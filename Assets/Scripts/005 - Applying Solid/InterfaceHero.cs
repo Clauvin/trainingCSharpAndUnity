10b6fd000760cebca66e57bcbb089f8366759454
Cláuvin <Clauvin@users.noreply.github.com>
2022-11-16 12:10:36 -0300

    Created InterfaceHero.cs

---
+++ Assets/Scripts/005 - Applying Solid/InterfaceHero.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface InterfaceHero
+{
+    public SolidXP GetSolidXP();
+
+    public SolidHP GetSolidHP();
+
+    public SolidDamage GetSolidDamage();
+
+}
