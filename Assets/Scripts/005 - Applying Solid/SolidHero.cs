ee863e6c962395f57cb357ea55fc67daea9cc297
Cláuvin <Clauvin@users.noreply.github.com>
2022-11-16 12:09:03 -0300

    Added SolidHP to SolidHero

--- Assets/Scripts/005 - Applying Solid/SolidHero.cs
+++ Assets/Scripts/005 - Applying Solid/SolidHero.cs
@@ -5,29 +5,52 @@ using UnityEngine;
 public class SolidHero
 {
     SolidXP solidXP;
+    SolidHP solidHP;
     SolidDamage solidDamage;
     
     public SolidHero()
     {
         solidXP = new SolidXP();
+        solidHP = new SolidHP();
         solidDamage = new SolidDamage();
     }
 
     public SolidHero(int dice, int face)
     {
         solidXP = new SolidXP();
+        solidHP = new SolidHP();
+        solidDamage = new SolidDamage(dice, face);
+    }
+
+    public SolidHero(int HP, int dice, int face)
+    {
+        solidXP = new SolidXP();
+        solidHP = new SolidHP(HP);
         solidDamage = new SolidDamage(dice, face);
     }
 
     public SolidHero(int[] dices, int[] faces)
     {
         solidXP = new SolidXP();
+        solidHP = new SolidHP();
+        solidDamage = new SolidDamage(dices, faces);
+    }
+
+    public SolidHero(int HP, int[] dices, int[] faces)
+    {
+        solidXP = new SolidXP();
+        solidHP = new SolidHP(HP);
         solidDamage = new SolidDamage(dices, faces);
     }
 
     public SolidXP GetSolidXP()
     {
         return solidXP;
+    }
+
+    public SolidHP GetSolidHP()
+    {
+        return solidHP;
     }
 
     public SolidDamage GetSolidDamage()
