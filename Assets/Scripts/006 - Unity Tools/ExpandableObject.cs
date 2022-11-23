890b35d1ad11fe687b23dd90da8b57b72554dfb0
Cláuvin <Clauvin@users.noreply.github.com>
2022-11-23 15:28:20 -0300

    007 is done, and I should've done this commit for 006 -_-

---
+++ Assets/Scripts/006 - Unity Tools/ExpandableObject.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExpandableObject : MonoBehaviour
+{
+
+    public string text;
+    public float float_value;
+    public int integer;
+
+}
